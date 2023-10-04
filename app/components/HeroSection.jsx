75ffe27ffa33d932acf9b030325c8f3e407db4c2
jcamp50 <7jacampbell@gmail.com>
2023-10-04 18:10:39 -0400

    header v1

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,33 +1,61 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
 
 const HeroSection = () => {
   return (
     <section>
-      <div className='grid grid-cols-1 lg:grid-cols-12'>
-        <div className='col-span-7 place-self-center'>
-          <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>
-            Hello, I&apos;m Jordan
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='col-span-7 place-self-center text-center sm:text-left'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-green-600'>
+              Hello, I&apos;m{' '}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Jordan.',
+                1500, // wait 1s before replacing "Mice" with "Hamsters"
+                'Jordan. Software Engineer.',
+                1500,
+                'Jordan. Front-End Developer.',
+                1500,
+                'Jordan. UI/UX Designer.',
+                1500,
+              ]}
+              wrapper='span'
+              speed={30}
+              repeat={Infinity}
+            />
           </h1>
-          <p className='text-[#ADB7BE] text-lg mb-6 lg:text-xl'>
+          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
             Placeholder paragraph add some infomation about myself and what I do
             etc
           </p>
           <div>
-            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>Contact Me</button>
-            <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>My Resume</button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-teal-500 to-green-500 hover:bg-slate-200 text-white'>
+              Contact Me
+            </button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-teal-500 to-green-500 hover:bg-slate-800 text-white mt-3'>
+              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+                My Resume
+              </span>
+            </button>
           </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-        <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+          <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
             <Image
-            src='/images/hero-image-laptop.png'
-            alt='hero image'
-            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-            width={300}
-            height={300}
+              src='/images/hero-image-laptop.png'
+              alt='hero image'
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              width={300}
+              height={300}
             />
-            </div>
+          </div>
         </div>
       </div>
     </section>
