4fa9423c2bd25e9f7441c7db5bca7318da88b9f0
jcamp50 <7jacampbell@gmail.com>
2023-10-18 18:10:21 -0400

    Activity section

---
+++ app/components/ActivitySection.jsx
@@ -0,0 +1,34 @@
+'use client';
+import React from 'react';
+import GithubContributions from './GithubContributions';
+import LinkedInFeed from './LinkedInFeed';
+import Slider from 'react-slick';
+
+const ActivitySection = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // update active slide index after slide change
+  };
+
+  return (
+    <section id='activity'>
+      <h2 className='flex text-4xl font-bold justify-center mt-10 text-white mb-6'>
+        Activity
+      </h2>
+      <Slider {...settings}>
+        <div>
+          <GithubContributions />
+        </div>
+        <div>
+          <LinkedInFeed />
+        </div>
+      </Slider>
+    </section>
+  );
+};
+
+export default ActivitySection;
