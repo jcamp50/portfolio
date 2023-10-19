d4e622c094afe8274d9c22c0a7e0a187b58bb6ea
jcamp50 <7jacampbell@gmail.com>
2023-10-19 01:56:48 -0400

    footer fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,8 +4,8 @@ import Logo from 'public/logo.png';
 const Footer = () => {
   return (
     <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-[#33353F]'>
-      <div className='container p-12 flex justify-between'>
-        <div className='text-lg md:text-2xl'>&lt;portfolio/&gt;</div>
+      <div className='container p-12 flex gap-2 justify-between'>
+        <div className='text-md md:text-2xl'>&lt;portfolio/&gt;</div>
         <p className='text-slate-600'>© Jordan Campbell 2023</p>
       </div>
     </footer>
