5d095326e1e1750e60aadb643816dfd88bc2ef34
jcamp50 <7jacampbell@gmail.com>
2025-09-09 14:54:13 -0400

    feat: arrow to scroll

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -3,17 +3,33 @@ import React from 'react';
 import Image from 'next/image';
 import heroLaptop from '@/public/images/hero-image-laptop.png';
 import ScrollPhrase from './ScrollPhrase';
+import ArrowDown from '@/public/ArrowDown.svg';
 
 const HeroSection = () => {
   return (
-    <div className='flex flex-col gap-16'>
-      <div className='flex flex-row items-center justify-around'>
+    <div className='flex flex-col gap-20'>
+      <div className='flex flex-row items-start'>
         {/* Left: Gradient Text */}
-        <div className='flex-1'>
-          <h1 className='text-9xl font-gestura text-gradient-clip leading-tight'>
-            Hey there, <br />
-            I&rsquo;m Jordan.
-          </h1>
+        <div className='flex-1 pt-48'>
+          <div className='inline-block text-left'>
+            <h1 className='text-9xl font-gestura text-gradient-clip leading-tight'>
+              Hey there, <br />
+              I&rsquo;m Jordan.
+            </h1>
+
+            <div className='mt-20 text-center'>
+              <p className='mt-4 text-[0.70rem] tracking-[0.24em] font-gestura uppercase font-light microcopy-reveal text-black/80'>
+                Scroll
+              </p>
+              <Image
+                src={ArrowDown}
+                alt='Scroll down'
+                className='inline-block arrow-reveal'
+                width={48}
+                height={48}
+              />
+            </div>
+          </div>
         </div>
         {/* Right: Red Background with Image */}
         <div className='flex-1 flex justify-end'>
